e0cc40b59e16590cfd075c7979b2c48242653dc0
Agsmods <136147260+Agsbotsyt@users.noreply.github.com>
2023-06-12 00:32:40 +0530

    Create file_renamer.py

---
+++ file_renamer.py
@@ -0,0 +1,68 @@
+import logging
+
+import os
+
+from telegram import Update
+
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+# Enable logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+# Define command handler
+
+def rename_files(update: Update, context: CallbackContext):
+
+    chat_id = update.effective_chat.id
+
+    message_text = update.message.text
+
+    # Assuming the message is in the format: "/rename old_name new_name"
+
+    command, old_name, new_name = message_text.split()
+
+    try:
+
+        os.rename(old_name, new_name)
+
+        context.bot.send_message(chat_id=chat_id, text="File renamed successfully!")
+
+    except FileNotFoundError:
+
+        context.bot.send_message(chat_id=chat_id, text="File not found.")
+
+    except:
+
+        context.bot.send_message(chat_id=chat_id, text="An error occurred while renaming the file.")
+
+# Set up the bot
+
+def main():
+
+    # Set up the Telegram Bot API token
+
+    bot_token = "YOUR_BOT_TOKEN"
+
+    # Create an instance of the Updater class with your bot token
+
+    updater = Updater(token=bot_token, use_context=True)
+
+    # Get the dispatcher to register handlers
+
+    dispatcher = updater.dispatcher
+
+    # Register the rename command handler
+
+    dispatcher.add_handler(CommandHandler('rename', rename_files))
+
+    # Start the bot
+
+    updater.start_polling()
+
+    updater.idle()
+
+if __name__ == '__main__':
+
+    main()
+
